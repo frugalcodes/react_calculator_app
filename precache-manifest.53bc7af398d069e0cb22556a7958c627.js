ab8bccaa2c3c63e0b8b6276eeb5c3aed5770d92e
Frugalcodes <samueligwe63@gmail.com>
2022-10-14 16:03:50 +0100

    Updates

---
+++ precache-manifest.53bc7af398d069e0cb22556a7958c627.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b288fca607ad736c7fa9",
+    "url": "/react_calculator_app/static/css/main.0dd38857.chunk.css"
+  },
+  {
+    "revision": "b288fca607ad736c7fa9",
+    "url": "/react_calculator_app/static/js/main.b288fca6.chunk.js"
+  },
+  {
+    "revision": "95f8b64eec29570e4f23",
+    "url": "/react_calculator_app/static/js/1.95f8b64e.chunk.js"
+  },
+  {
+    "revision": "d0c549322066cda73f1a",
+    "url": "/react_calculator_app/static/js/runtime~main.d0c54932.js"
+  },
+  {
+    "revision": "35bf7e0cc2edb222a4fcc2b32dde83b4",
+    "url": "/react_calculator_app/index.html"
+  }
+];
